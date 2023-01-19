74e4e10d65cba7a5f6569ddd1e93f9831468343c
CharlieVM <charlievanrooijen@gmail.com>
2023-01-19 16:34:56 +0100

    First major version

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,9 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+use App\Repository\AccountRepository;
+
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
+    #[Route(path: '/login', name: 'app_login', methods: ['GET', 'POST'])]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
          if ($this->getUser()) {
